dd021a95139455e8d80c3d3c1646197c0e99fa93
Neish Kamani <neish.kamani125722@marwadiuniversity.ac.in>
2026-07-17 08:19:59 +0530

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewpoint" content="width-device-width,initial-scale=1.0">
+        <title>Document</title>
+</head>
+<body>
+
+<!--Name is for server (PHP,Python,Java)Variables-->
+<!--Id is for Client(JavaScript)Variables-->
+
+    <form action="login.php" method="get">
+        <input type="text" name="f_name" id="" placeholder="firstname">
+        <input type="text" name="s_name" id="" placeholder="Surname">
+        <input type="submit">
+        <input type="reset'>
+</form>
+
+</body>
+</html>
